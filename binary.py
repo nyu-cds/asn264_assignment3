14b83ec4849b65374fe75ba266964bc34a31eb05
Aditi Nair <asn264@nyu.edu>
2017-02-26 00:12:59 -0500

    change print to return

--- binary.py
+++ binary.py
@@ -12,43 +12,34 @@ one per line.
 import itertools
 import sys
 
-class InvalidZbitsException(Exception):
-
-	'''This is specifically for inputs n and k which do not satisfy 0<=k<=n.'''
-	pass
-
-
 def zbits(n,k):
 	
 	'''
-	First we ensure the following inequality holds: 0 <= k <= n.
+	First we ensure the following inequality holds (0 <= k <= n) and the values are ints.
 	If this is true, we create a "master string" which contains a single binary string of length n 
 	with k zero bits - here we use ITERTOOLS.CHAIN.
 	
 	Next we use ITERTOOLS.PERMUTATIONS to get all possible permutations of characters in master_str.
 	However, ITERTOOLS.PERMUTATIONS is position-based so master_str='00' will have two permutations, 
-	not one. Therefore we apply the set function to the iterator before printing the results.
+	not one. Therefore we apply the set function to the iterator before returning the results.
 	'''
 
-	if k<=n and k>=0: #check input
+	if isinstance(k,int) and isinstance(n,int) and k<=n and k>=0: #check input
 		master_str=''.join([i for i in itertools.chain(k*'0',(n-k)*'1')]) #create a "master string"
-		for bin_str in set(itertools.permutations(master_str)): #generate all 'unique' permutations
-			print ''.join(i for i in bin_str)
+		return {''.join(bin_str) for bin_str in itertools.permutations(master_str)} #generate all 'unique' permutations			
 	else:
-		raise InvalidZbitsException
+		return 'Invalid Input.'
 
 
 
 def main():
 	
-	query = raw_input('Please enter two positive integers n and k, separated by a comma.\n')
+	import binary
 	
-	try:
-		n,k = map(int,query.strip().split(','))
-		zbits(n,k)
-	except (ValueError,InvalidZbitsException):
-		print 'Invalid input.'
-		sys.exit()
+	assert binary.zbits(4, 3) == {'0100', '0001', '0010', '1000'}
+	assert binary.zbits(4, 1) == {'0111', '1011', '1101', '1110'}
+	assert binary.zbits(5, 4) == {'00001', '00100', '01000', '10000', '00010'}
+
 
 
 if __name__ == '__main__':
